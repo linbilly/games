391681594881e4cf363c2374f1524608d2c162c4
linbilly <linbilly@users.noreply.github.com>
2026-02-26 20:04:12 -0800

    move vanish to a different repo

--- vanish/ios/App/App/GameCenterPlugin.m
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(GameCenterPlugin, "GameCenterPlugin",
-           CAP_PLUGIN_METHOD(signIn, CAPPluginReturnPromise);
-)
